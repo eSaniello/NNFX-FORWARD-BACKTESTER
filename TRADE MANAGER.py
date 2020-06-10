18aaa4780caa424305cb5757983ffa3a2680bf92
eSaniello <shaniel29samadhan@gmail.com>
2020-06-10 02:05:34 -0300

    Making some progress

--- TRADE MANAGER.py
+++ TRADE MANAGER.py
@@ -11,24 +11,65 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+pub = context.socket(zmq.PUB)
+pub.bind("tcp://*:6666")
+
+everyone_conected = False
+json_msg = None
+
 while True:
+    counter = 0
     #  Wait for next request from client
-    message = socket.recv()
-    json_msg = json.loads(message)
-    print(json_msg)
+    while True:
+        message = socket.recv()
+        json_msg = json.loads(message)
+        print(json_msg)
 
-    # Check for data and send reply
+        if json_msg != None:
+            counter = counter + 1
+            socket.send(b"OK")
+            print(json_msg["symbol"], "conncected")
+
+            if counter >= 1:
+                print("everyone connected")
+                break
+
+    # if everyone_conected == False:
+    #     message = socket.recv()
+    #     json_msg = json.loads(message)
+    #     print(json_msg)
+
+    #     if json_msg != None:
+    #         counter = counter + 1
+    #         socket.send(b"OK")
+    #         print(json_msg["symbol"], "conncected")
+
+    #         if counter >= 1:
+    #             print("everyone connected")
+    #             everyone_conected = True
+
+    # if everyone_conected == True:
+        # Check for data and send reply
+
+    # message = socket.recv()
+    # json_msg = json.loads(message)
+    # print(json_msg)
+
     if json_msg["signal"] == "FLAT":
-        socket.send(b"FLAT")
+        topic = json_msg["symbol"]
+        message_data = "hi"
+        pub.send_string("%s %s" % (topic, message_data))
+        # socket.send(b"OK")
+        # print('signal sent', "%s %s" % (topic, message_data))
 
-    if json_msg["signal"] == "SHORT" and json_msg["open_orders"] == "0":
-        socket.send(b"SHORT")
+        # if json_msg["signal"] == "SHORT" and json_msg["open_orders"] == "0":
+        #     socket.send(b"SHORT")
 
-    if json_msg["signal"] == "SHORT" and json_msg["open_orders"] != "0":
-        socket.send(b"FLAT")
+        # if json_msg["signal"] == "SHORT" and json_msg["open_orders"] != "0":
+        #     socket.send(b"FLAT")
 
-    if json_msg["signal"] == "LONG" and json_msg["open_orders"] == "0":
-        socket.send(b"LONG")
+        # if json_msg["signal"] == "LONG" and json_msg["open_orders"] == "0":
+        #     socket.send(b"LONG")
 
-    if json_msg["signal"] == "LONG" and json_msg["open_orders"] != "0":
-        socket.send(b"FLAT")
+        # if json_msg["signal"] == "LONG" and json_msg["open_orders"] != "0":
+        #     socket.send(b"FLAT")
