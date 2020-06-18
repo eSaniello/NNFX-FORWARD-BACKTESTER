2b25155c8956faf70349af907d0f47c3b508656f
eSaniello <shaniel29samadhan@gmail.com>
2020-06-18 00:45:33 -0300

    Working on running multiple mt4's from python

---
+++ copy_testers.py
@@ -0,0 +1,8 @@
+import shutil
+
+forex_pairs = ["AUDCAD", "AUDCHF", "AUDJPY", "AUDNZD", "AUDUSD", "CADCHF", "CADJPY", "CHFJPY", "EURCHF", "EURAUD", "EURCAD", "EURGBP", "EURJPY", "EURNZD", "EURUSD",
+               "GBPAUD", "GBPCAD", "GBPCHF", "GBPJPY", "GBPNZD", "GBPUSD", "NZDCHF", "NZDCAD", "NZDJPY", "NZDUSD", "USDCAD", "USDCHF", "USDJPY"]
+
+for pair in forex_pairs:
+    shutil.copytree('portable mt4/', f'testers/{pair}/', True)
+    print(f'Copied {pair}')
